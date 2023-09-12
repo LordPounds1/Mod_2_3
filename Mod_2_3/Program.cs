7a8caec5c40fa50886e0887290dc575dc88350d4
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 21:02:35 +0600

    Add project files.

---
+++ Mod_2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mod_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 9;
+
+            Console.WriteLine("Таблица умножения на " + number + ":");
+
+            for (int i = 1; i <= 9; i++)
+            {
+                int result = number * i;
+                Console.WriteLine(number + " x " + i + " = " + result);
+            }
+        }
+    }
+}
